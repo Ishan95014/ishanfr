97ead0d99b223d224884bd2dc82894932b32ce5d
Ishan95014 <59368580+Ishan95014@users.noreply.github.com>
2023-02-27 23:26:33 +0100

    fixes to js - use fs

--- experiments.js
+++ experiments.js
@@ -1,62 +1,48 @@
-// For each html file in the experiments folder, create a new section in the experiments.html file
-// with an h1 tag conatining the h1 tag in the experiement hmlt file and a p tag containing the p tag in the experiment html file with id "short-description"
-// and a link to the experiment html file
+const fs = require('fs');
 
 // Get the experiments folder
-var experimentsFolder = new Folder("experiments");
+const experimentsFolder = './experiment_folder';
 
 // Get the list of files in the experiments folder
-var experimentsFiles = experimentsFolder.getFiles();
+const experimentsFiles = fs.readdirSync(experimentsFolder);
 
 // Get the experiments.html file
-var experimentsHtmlFile = new File("experiments.html");
+const experimentsHtmlFile = './experiments.html';
 
 // Open the experiments.html file
-experimentsHtmlFile.open("w");
+fs.writeFileSync(experimentsHtmlFile, '');
 
 // For each file in the experiments folder,
 // create a new section in the experiments.html file and fill it with the h1 tag and p tag from the experiment html file
 
-for (var i = 0; i < experimentsFiles.length; i++) {
+for (let i = 0; i < experimentsFiles.length; i++) {
   // Get the current file
-  var currentFile = experimentsFiles[i];
+  const currentFile = experimentsFiles[i];
 
   // Get the name of the current file
-  var currentFileName = currentFile.name;
+  const currentFileName = currentFile.name;
 
   // If the current file is an html file
   if (currentFileName.indexOf(".html") != -1) {
     // Get the html file
-    var htmlFile = new File(currentFile);
-
-    // Open the html file
-    htmlFile.open("r");
+    const htmlFile = fs.readFileSync(`${experimentsFolder}/${currentFile}`);
 
     // Get the html file contents
-    var htmlFileContents = htmlFile.read();
-
-    // Close the html file
-    htmlFile.close();
+    const htmlFileContents = htmlFile.toString();
 
     // Get the h1 tag in the html file
-    var h1Tag = htmlFileContents.match(/<h1>(.*?)<\/h1>/)[1];
+    const h1Tag = htmlFileContents.match(/<h1>(.*?)<\/h1>/)[1];
 
     // Get the p tag in the html file with id "short-description"
-    var pTag = htmlFileContents.match(
+    const pTag = htmlFileContents.match(
       /<p id="short-description">(.*?)<\/p>/
     )[1];
 
     // Create the section for the current experiment
-    var section =
-      "<section><h1>" +
-      h1Tag +
-      "</h1><p>" +
-      pTag +
-      "</p><a href='" +
-      currentFileName +
-      "'>View Experiment</a></section>";
+    const section =
+      `<section><h1>${h1Tag}</h1><p>${pTag}</p><a href='${currentFileName}'>View Experiment</a></section>`;
 
     // Write the section to the experiments.html file
-    experimentsHtmlFile.writeln(section);
+    fs.appendFileSync(experimentsHtmlFile, section);
   }
 }
